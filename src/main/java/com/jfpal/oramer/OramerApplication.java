fb92790cbb4aa928a93a120ec38560a8485113cd
刘康 <lk@jfpal.com>
2017-10-09 14:48:26 +0800

    代码提交

---
+++ src/main/java/com/jfpal/oramer/OramerApplication.java
@@ -0,0 +1,12 @@
+package com.jfpal.oramer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OramerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OramerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jfpal/oramer/OramerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfpal.oramer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OramerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
